d46226f35faebc91b5e83f817f630877328bf5f7
Amir <amir.yakdi@gmail.com>
2024-10-31 11:49:28 +0100

    day4.1

--- elements/styles/GlobalStyles.js
+++ elements/styles/GlobalStyles.js
@@ -1,26 +1,35 @@
 //yarn add react-native-shadow-2
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Platform, Dimensions } from 'react-native';
 import Colors from './Colors';
 import Typography from './Typography';
 
 export default StyleSheet.create({
 
+    //app general style
+    appStyle: {
+        flex: 1,
+        backgroundColor: Colors.background,
+        justifyContent: 'center', 
+        align: 'center',
+    },
+
 //_________________CONTAINERS_________________
+
+
 
   // Global container for all screens with consistent padding
   screenMainContainer: {
     flex: 1,
     backgroundColor: Colors.background,
     paddingHorizontal: 36,  
-    paddingVertical: 36,   
     justifyContent: 'top', //top alignment for all content
     align: 'center',
     display: 'flex',
     
   },
 
-  //white main container with shadow
-  whiteContainer: {
+//white main container with shadow
+whiteContainer: {
     backgroundColor: Colors.whiteColor,
     borderRadius: 12,
     
@@ -37,16 +46,21 @@ export default StyleSheet.create({
     shadowColor: Colors.shadow,
     elevation: Platform.OS === 'android' ? 20 : 0,
     shadowColor: Platform.OS === 'android' ? Colors.shadowAndroid : Colors.shadow,
-  },
+},
 
     //white card container with shadow (for information cards mainly for items)
-    whiteCardContainer: {
+whiteCardContainer: {
     backgroundColor: Colors.whiteColor,
-    borderRadius: 12,
+    borderRadius: 16,
+    
         
     //spaces
-    padding: 12,
+    paddingHorizontal: 36,
+    paddingVertical: 24,
     marginBottom: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     
     // iOS Shadow
     shadowColor: Colors.shadow,
@@ -57,8 +71,75 @@ export default StyleSheet.create({
     shadowColor: Colors.shadow,
     elevation: Platform.OS === 'android' ? 20 : 0,
     shadowColor: Platform.OS === 'android' ? Colors.shadowAndroid : Colors.shadow,
+},
+
+CardTextContainer: {
+    width: '60%',
+    margin: -4,
+        
+},
+
+WelcomeContainer: {
+    marginBottom: 36,
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    display: 'flex',
+    direction: 'horizontal',
+
+},
+
+PictureProfileContainer: {
+    backgroundColor: Colors.shadow,
+    borderRadius: 100,
+    marginRight: 24,
+    alignItems: 'center',
+    justifyContent: 'center', 
+    width: 70,
+    height: 70,
+},
+
+GreyCircleButton: {
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 4, 
+},
+
+    //TabNavigator container with shadow 
+    navigatorContainer: {
+        backgroundColor: Colors.whiteColor,
+        width: Dimensions.get('window').width*0.9,
+        borderRadius: 40,
+        zIndex: 3,
+            
+        //spaces
+        marginBottom: 24,
+        alignItems: 'center',
+
+        
+        // iOS Shadow
+        shadowColor: Colors.shadow,
+        shadowOpacity: 100,
+        shadowRadius: 80,
+            
+        // Android Shadow
+        shadowColor: Colors.shadow,
+        elevation: Platform.OS === 'android' ? 20 : 0,
+        shadowColor: Platform.OS === 'android' ? Colors.shadowAndroid : Colors.shadow,
     },
 
+        //TabNavigator inside container with shadow 
+        navigatorInnerContainer: {
+            width: '90%',
+            //spaces
+            padding: 12,
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+
+            
+        },
 
     //For forms (signin/signup/etc...)
     whiteFormContainer: {
@@ -151,7 +232,7 @@ export default StyleSheet.create({
         shadowColor: Platform.OS === 'android' ? Colors.shadowAndroid : Colors.shadow,
     },
 
-      //white main container with shadow
+    //white main container with shadow
     requestContainer: {
     backgroundColor: Colors.whiteColor,
     borderRadius: 12,
@@ -177,12 +258,27 @@ export default StyleSheet.create({
     shadowColor: Platform.OS === 'android' ? Colors.shadowAndroid : Colors.shadow,
   },
 
+    //_________________HEADERS WITH BACKGROUND COLOR & ICONS_________________
+
+    coloredHeader: {
+        backgroundColor: Colors.purpleColor, // Default color
+        borderBottomRightRadius: 60,
+        paddingTop: 60,
+        marginBottom: 36,
+        alignItems: 'left',
+        flexDirection: 'row',
+        paddingHorizontal: 16,
+        justifyContent: 'left',
+        height: 140,
+      },
+
     //_________________TITLES_________________
     //H1 black color
     headerTextBlack: {
         ...Typography.h1,
         color: Colors.textColor,
         paddingVertical: 24,
+        marginBottom: -30,
     },
 
     //H1 white color
@@ -190,6 +286,9 @@ export default StyleSheet.create({
         ...Typography.h1,
         color: Colors.whiteColor,
         paddingVertical: 24,
+        marginLeft: 12,
+        marginRight: 12,
+        marginBottom: -30,
     },
 
     //H1 purple color
@@ -197,6 +296,7 @@ export default StyleSheet.create({
         ...Typography.h1,
         color: Colors.purpleColor,
         paddingVertical: 24,
+        marginBottom: -30,
     },
 
     //H1 green color
@@ -204,6 +304,7 @@ export default StyleSheet.create({
         ...Typography.h1,
         color: Colors.greenColor,
         paddingVertical: 24,
+        marginBottom: -30,
     },
 
     //H1 red color
@@ -211,6 +312,7 @@ export default StyleSheet.create({
     ...Typography.h1,
     color: Colors.redColor,
     paddingVertical: 24,
+    marginBottom: -30,
     },
 
     //H2 black color
@@ -372,12 +474,11 @@ export default StyleSheet.create({
         marginVertical: 8,
     },
 
-
   buttonPrimary: {
     backgroundColor: Colors.purpleColor, // Default color
     borderRadius: 60,
     paddingVertical: 4,
-    marginVertical: 36,
+    marginVertical: 24,
     alignItems: 'center',
 
   },
@@ -391,7 +492,7 @@ export default StyleSheet.create({
   buttonSecondary: {
     borderRadius: 60,
     paddingVertical: 4,
-    marginVertical: 36,
+    marginVertical: 24,
     alignItems: 'center',
     //stoke color
     borderWidth: 1.5,
@@ -404,7 +505,35 @@ export default StyleSheet.create({
 
   },
 
+  buttonTextBlack: {
+    ...Typography.cta,
+    color: Colors.textColor,
 
+  },
+
+    buttonTextPurple: {
+    ...Typography.cta,
+    color: Colors.purpleColor,
+
+    },
+
+    buttonTextGreen: {
+        ...Typography.cta,
+        color: Colors.greenColor,
+    
+    },
+
+    buttonTextWhite: {
+        ...Typography.cta,
+        color: Colors.whiteColor,
+    
+    },
+
+  iconText: {
+    ...Typography.paragraphSmallGrey,
+    color: Colors.textColor,
+    marginTop: 2,
+  },
 
 
 });
